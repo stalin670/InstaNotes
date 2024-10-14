afa0c9eb12bd81d9e1a1a0d1d5969d3bf389ba98
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-14 13:30:04 +0530

    adding notecard in home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title={"Date on 15th October"}
+            date={"14th Oct 2024"}
+            content={"Date on 15th October in Delhi"}
+            tags={"#meeting"}
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {}}
+      >
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+    </>
   );
 };
 
