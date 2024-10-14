9600b734aa438b93848a7c9fcc8dd492875e613b
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-14 19:43:43 +0530

    note updatation api created

--- backend/server.js
+++ backend/server.js
@@ -163,6 +163,40 @@ app.post("/add-note", authenticateToken, async (req, res) => {
   }
 });
 
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
+  try {
+    const noteId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const user = req.user;
+
+    if (!title && !content && !tags) {
+      return res
+        .status(400)
+        .json({ error: true, message: "No changes provided" });
+    }
+
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found" });
+    }
+
+    if (title) note.title = title;
+    if (content) note.content = content;
+    if (tags) note.tags = tags;
+    if (isPinned) note.isPinned = isPinned;
+
+    await note.save();
+
+    return res
+      .status(200)
+      .json({ error: false, note, message: "Note updated successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: false, note, message: "Internal server error" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
