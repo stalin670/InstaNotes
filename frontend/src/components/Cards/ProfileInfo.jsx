e98b4218be9cdaf42fce6ba1411c07b9abaf4a6a
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-15 14:35:15 +0530

    Done : getUser api integration

--- frontend/src/components/Cards/ProfileInfo.jsx
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { getInitials } from "../../utils/helper";
 
-const ProfileInfo = ({ onLogout }) => {
+const ProfileInfo = ({ userInfo, onLogout }) => {
+  console.log(userInfo?.fullname);
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
-        {getInitials("Amit Yadav")}
+        {getInitials(userInfo?.fullname)}
       </div>
       <div>
-        <p className="text-sm font-medium">Amit</p>
+        <p className="text-sm font-medium">{userInfo?.fullname}</p>
         <button className="text-sm text-slate-700 underline" onClick={onLogout}>
           Logout
         </button>
