90b1d8efe18cfe7f5331835b4e2a36d3e5b8e2ab
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-15 21:02:07 +0530

    created search api at backend

--- backend/server.js
+++ backend/server.js
@@ -309,6 +309,40 @@ app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
   }
 });
 
+// Search note
+app.get("/search-notes/", authenticateToken, async (req, res) => {
+  try {
+    const user = req.user;
+    const { query } = req.query;
+
+    if (!query) {
+      return res.status(400).json({
+        error: true,
+        message: "Search query is required",
+      });
+    }
+
+    const matchingNotes = await Note.find({
+      userId: user._id,
+      $or: [
+        { title: { $regex: new RegExp(query, "i") } },
+        { content: { $regex: new RegExp(query, "i") } },
+      ],
+    });
+
+    return res.status(200).json({
+      error: false,
+      notes: matchingNotes,
+      message: "Notes matching the search query retrieved successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal Server Error",
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
