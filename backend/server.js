cf998123d3683555a0a8ce6fcf4443f045f8e6e0
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-19 16:15:05 +0530

    fixing bugs

--- backend/server.js
+++ backend/server.js
@@ -352,10 +352,10 @@ app.get("/search-notes/", authenticateToken, async (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
